b14cfc8f08aad4aba2ff25e439753f26cd7179e3
James <jlhaley1@gmail.com>
2013-07-10 17:10:31 -0700

    week view working

--- app/assets/javascripts/models/calendar.js
+++ app/assets/javascripts/models/calendar.js
@@ -8,28 +8,3 @@ Cal.Models.Calendar = Backbone.RelationalModel.extend({
   }]
 
 });
-
-
-// parse: function (json) {
-//   json = json.calendar;
-//   if (!this.events) {
-//     this.events = new Cal.Collections.Events({
-//       event: this
-//     });
-//   }
-// 
-//   this.events.set(json.events);
-//   delete json.events;
-// 
-//   return json;
-// },
-// 
-// 
-// toJSON: function (options) {
-//   var json = Backbone.Model.prototype.toJSON.call(this, options);
-// 
-//   json.gist.gist_files_attributes = this.gistFiles.toJSON();
-//   delete json.gist.favorited;
-// 
-//   return json;
-// }

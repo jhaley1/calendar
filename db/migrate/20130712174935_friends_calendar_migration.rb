c4b9821788da07a356977b6bcb622a9b73716fa6
James <jlhaley1@gmail.com>
2013-07-12 10:52:23 -0700

    fixing stuffs

---
+++ db/migrate/20130712174935_friends_calendar_migration.rb
@@ -0,0 +1,11 @@
+class FriendsCalendarMigration < ActiveRecord::Migration
+  def change
+    create_table :friends_calendars do |t|
+      t.integer :friend_id
+      t.integer :shared_calendar_id
+
+      t.timestamps
+    end
+    
+  end
+end
